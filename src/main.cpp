6dd907be1f4315abc0c6d4f9a643b8aad3f7bff0
willy0483 <willyjensen251@gmail.com>
2025-12-20 01:24:20 +0100

    added a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <math.h>
 
+#include "shader.h"
+
 const unsigned int width = 800;
 const unsigned int height = 600;
 
@@ -43,6 +45,37 @@ int main()
 		return -1;
 	}
 
+	// build and compile our shader program
+	Shader shader("src/Shaders/default.vert", "src/Shaders/default.frag");
+
+	float vertices[] = {
+		-0.5f, -0.5, 0.0f, // bottom left
+		0.5f,  -0.5, 0.0f, // bottom right
+		0.0f,  0.5,	 0.0f, // top
+	};
+
+	unsigned int VAO;
+	unsigned int VBO;
+
+	// generate
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// bind - data
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// attributes
+	// positions
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// unbind
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	// main loop
 	while(!glfwWindowShouldClose(window))
 	{
@@ -51,10 +84,23 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// render
+		shader.use();
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
+
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// delete objects
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// delete shaders;
+	shader.deleteProgram();
 
 	// clearing all previously allocated GLFW resources
 	glfwTerminate();
