4b66b2df6ca8885c808b453a0430c048d70a268a
willy0483 <willyjensen251@gmail.com>
2025-12-20 02:53:59 +0100

    added color to default shaders

--- src/main.cpp
+++ src/main.cpp
@@ -49,17 +49,21 @@ int main()
 	Shader shader("src/Shaders/default.vert", "src/Shaders/default.frag");
 
 	float vertices[] = {
-		-0.5f, -0.5, 0.0f, // bottom left
-		0.5f,  -0.5, 0.0f, // bottom right
-		0.0f,  0.5,	 0.0f, // top
+		-0.5f, -0.5, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+		0.5f,  -0.5, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+		0.0f,  0.5,	 0.0f, 1.0f, 0.0f, 0.0f, // top
 	};
+
+	unsigned int indices[] = { 0, 1, 2 };
 
 	unsigned int VAO;
 	unsigned int VBO;
+	unsigned int EBO;
 
 	// generate
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// bind - data
 	glBindVertexArray(VAO);
@@ -67,14 +71,22 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// attributes
 	// positions
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// unbind
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// main loop
 	while(!glfwWindowShouldClose(window))
@@ -88,7 +100,7 @@ int main()
 		shader.use();
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -98,6 +110,7 @@ int main()
 	// delete objects
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	// delete shaders;
 	shader.deleteProgram();
